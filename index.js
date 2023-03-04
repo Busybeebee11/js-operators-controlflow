d4f7b670aa3f4ad94080418c3c80ce635ba1be0f
Busybeebee11 <105250713+Busybeebee11@users.noreply.github.com>
2023-03-04 12:57:16 +0100

    review and amendment

--- index.js
+++ index.js
@@ -3,31 +3,45 @@
 
 // IF...ELSE CONDITIONAL STATEMENT
 
-let classGroup = "Arts"; // Class group to determine applicable subjects
+let classGroup = ""; // Class group to determine applicable subjects
 
 let scienceSubjects = "Physics, Chemistry, Biology, Technical Drawing";
 let socialScienceSubjects = "Accounting, Commerce, Marketing, Geography";
 let artsSubjects = "Government, Economics, Literature, History";
 let generalSubjects = "English, Mathematics";
 
-if (classGroup === "Science") { // if...else conditional statement used to determine subjects based on class group selection
-  console.log(
-    "You will be taking Physics, Chemistry, Biology, and Technical Drawing"
-  );
-} else if (classGroup === "Social Science") { 
-  console.log(
-    "You will be taking Accounting, Commerce, Marketing, and Geography"
-  );
-} else if (classGroup === "Arts") { 
-  console.log(
-    "You will be taking Government, Economics, Literature, and History"
-  );
-} else { // default - when none of the conditions are met or classGroup string is empty
-  console.log(
-    "Invalid class group. You will be taking the General Subjects: English and Mathematics"
-  );
-}
+// WHAT I SUBMITTED
+// if (classGroup === "Science") { // if...else conditional statement used to determine subjects based on class group selection
+//   console.log(
+//     "You will be taking Physics, Chemistry, Biology, and Technical Drawing"
+//   );
+// } else if (classGroup === "Social Science") { 
+//   console.log(
+//     "You will be taking Accounting, Commerce, Marketing, and Geography"
+//   );
+// } else if (classGroup === "Arts") { 
+//   console.log(
+//     "You will be taking Government, Economics, Literature, and History"
+//   );
+// } else { // default - when none of the conditions are met or classGroup string is empty
+//   console.log(
+//     "Invalid class group. You will be taking the General Subjects: English and Mathematics"
+//   );
+// }
 
+// REVIEWED AND FEEDBACK - made repetition obsolete and code easier and cleaner
+if (classGroup === "Science") { // if...else conditional statement used to determine subjects based on class group selection
+  console.log("You will be taking " + generalSubjects + ", " + scienceSubjects);
+
+} else if (classGroup === "Social Science") {
+  console.log("You will be taking " + generalSubjects + ", " + socialScienceSubjects);
+  
+} else if (classGroup === "Arts") {
+   console.log("You will be taking " + generalSubjects + ", " + artsSubjects);
+
+} else { // default - when none of the conditions are met or classGroup string is empty
+  console.log("Invalid class group. You will be taking the General Subjects: " + generalSubjects);
+}
 
 
 // QUESTION 5
@@ -42,8 +56,10 @@ while (pwr <= num) { // uses a while loop to keep doubling pwr until it exceeds 
   pwr *= 2;
 }
 
-if (pwr - num > num - pwr / 2) { // checks if the difference between pwr and num is greater than the difference between num and pwr/2. If so, it halves pwr to get the smaller power of 2 nearest to num.
+if (pwr - num >= num - pwr / 2) { // checks if the difference between pwr and num is greater than the difference between num and pwr/2. If so, it halves pwr to get the smaller power of 2 nearest to num.
   pwr /= 2;
 }
 
 console.log("The number " + pwr + " is the power of 2 nearest to " + num + ".");
+
+// Submitted the if condition with >, after review amended to >= to meet all the stated conditions
