2b55615dfb9cbf1d8b2d5d17b23438fca5086dc1
Busybeebee11 <105250713+Busybeebee11@users.noreply.github.com>
2023-03-09 14:14:28 +0100

    added mentor's solution to question 5

--- index.js
+++ index.js
@@ -63,3 +63,44 @@ if (pwr - num >= num - pwr / 2) { // checks if the difference between pwr and nu
 console.log("The number " + pwr + " is the power of 2 nearest to " + num + ".");
 
 // Submitted the if condition with >, after review amended to >= to meet all the stated conditions
+
+
+
+// SOLUTION TO QUESTION 5 BY MENTOR
+// Problem Breakdown
+// Input - a postive number, num
+// Output - power of 2 nearest to num, pwr
+
+// Test Cases
+// - if num = 3, pwr = 2
+// - If num = 40, pwr = 32
+// - If num = 50, pwr = 64
+
+// Pseudocode / Logic / Conditions
+// get input
+// set initial power, pwr = 0
+// set index = 0
+// while 2**index < num, set lastpower == 2**index. Then, keep doing 2**index
+// if power > num , set pwr  = new power
+// if new power - number >= num - lastpower, then pwr = lastpower
+
+let numSol = 40;
+
+let pwrSol = 0;
+let index = 0;
+let previousPower = 0;
+
+while (pwrSol < numSol) {
+  previousPower = pwrSol;
+  pwrSol = 2**index;
+  index++
+}
+
+let diff1 = numSol - previousPower; // 50 - 32 = 18
+let diff2 = pwrSol - numSol;  // 64 - 50 = 14
+
+if (diff2 >= diff1) {
+  pwrSol = previousPower  // 64
+}
+
+console.log("The number " + pwrSol + " is the power of 2 nearest to " + numSol);
